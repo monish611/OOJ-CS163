8289f792bc1026d2b50ae47763c30668b57aff55
monish611 <156738838+monish611@users.noreply.github.com>
2024-02-21 18:03:36 +0530

    Update Package.java

--- 6/Package.java
+++ 6/Package.java
@@ -19,3 +19,74 @@ public void display(){
 System.out.println("Name: " + this.name + "\nUSN: " + this.usn + "\nSem: " + this.sem);
 }
 }
+
+//file Internal.java in AIsh/CIE
+package CIE;
+import java.util.Scanner;
+public class Internal extends CIE.Student{
+public int m[] = new int[5];
+CIE.Student student = new CIE.Student();
+public void accept(){
+student.accept();
+Scanner s1 = new Scanner(System.in);
+System.out.println("Enter Internal Marks:");
+for(int i=0;i<5;i++){
+m[i] = s1.nextInt();
+}
+}
+public void display(){
+student.display();
+for(int i=0;i<5;i++){
+System.out.println("Marks of sub" + (i+1) + " = " + m[i]);
+}
+}
+}
+
+//file External.java in AIsh/SEE
+package SEE;
+import java.util.Scanner;
+import CIE.Internal;
+import CIE.Student;
+public class External extends CIE.Student{
+public int x[] = new int[5];
+public void accept(){
+Scanner s2 = new Scanner(System.in);
+System.out.println("Enter External Marks:");
+for(int i=0;i<5;i++){
+x[i] = s2.nextInt();
+}
+}
+public void display(){
+super.display();
+for(int i=0;i<5;i++){
+System.out.println("Marks of sub" + (i+1) + " = " + x[i]);
+}
+}
+}
+
+//file Final.java in Aish
+import java.util.Scanner;
+import CIE.Student;
+import CIE.Internal;
+import SEE.External;
+public class Final{
+public static void main(String[] args) {
+Scanner n = new Scanner(System.in);
+System.out.println("Enter n:");
+int y = n.nextInt();
+CIE.Internal[] c1 = new CIE.Internal[y];
+SEE.External[] c2 = new SEE.External[y];
+for(int i=0;i<y;i++){
+c1[i] = new CIE.Internal();
+c2[i] = new SEE.External();
+c1[i].accept();
+c2[i].accept();
+// c1[i].accept();c2[i].accept();
+c1[i].display();c2[i].display();
+for(int j=0;j<5;j++){
+double calc = c1[i].m[j]+((c2[i].x[j])/2);
+System.out.println("Final marks of sub["+(i+1)+"]= "+calc);
+}
+}
+}
+}
